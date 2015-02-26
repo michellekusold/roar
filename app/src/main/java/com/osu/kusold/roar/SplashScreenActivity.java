69d6f25853e6fa35e3394d58177a3a36f39421fd
Michelle Kusold <kusrox@gmail.com>
2015-02-26 11:26:47 -0500

    more work done on splashscreen to login transitions and created a separate activity for signup and login.

--- app/src/main/java/com/osu/kusold/roar/SplashScreenActivity.java
+++ app/src/main/java/com/osu/kusold/roar/SplashScreenActivity.java
@@ -1,16 +1,14 @@
 package com.osu.kusold.roar;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class SplashScreenActivity extends ActionBarActivity {
+public class SplashScreenActivity extends Activity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +26,21 @@ public class SplashScreenActivity extends ActionBarActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashScreenActivity.this, EventFeedActivity.class);
+                int deleteMeforLogic = 1;
+                Intent i;
+
+                // make logic if new user: signup
+                if(deleteMeforLogic == 0) {
+                    i = new Intent(SplashScreenActivity.this, SignUpActivity.class);
+                }
+                else if(deleteMeforLogic == 1) {
+                    // if not logged in, sign in screen
+                    i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                }
+                else {
+                    // else (not new user and logged in) eventFeed
+                    i = new Intent(SplashScreenActivity.this, EventFeedActivity.class);
+                }
 
                 startActivity(i);
 
@@ -39,26 +51,4 @@ public class SplashScreenActivity extends ActionBarActivity {
 
     }
 
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
 }
