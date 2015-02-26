6a606651612df006055ebdca6c05833d4dcf83a6
Michelle Kusold <kusrox@gmail.com>
2015-02-25 23:21:55 -0500

    Had insane stupid problems with getting splash screen to run. The problem ended up being it couldn't find the background color "white" *facepalm*. It should run now on a timer of 3 seconds and then load the eventFeed just as a test. Also I added icons.

--- app/src/main/java/com/osu/kusold/roar/SplashScreenActivity.java
+++ app/src/main/java/com/osu/kusold/roar/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package com.osu.kusold.roar;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,14 +10,37 @@ import android.view.MenuItem;
 
 public class SplashScreenActivity extends ActionBarActivity {
 
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, EventFeedActivity.class);
+
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
@@ -35,5 +60,5 @@ public class SplashScreenActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
