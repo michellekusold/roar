577dfd3dd9aa9f982ec8c36be2de135b2960bee4
SeanNicely <snicely93@gmail.com>
2015-03-27 18:22:57 -0400

    CreateEvent Now includes Top Bar and Drawer
    The content is overlapping with the top bar. I'm not really sure how to
    fix this.

--- app/src/main/java/com/osu/kusold/roar/CreateEventActivity.java
+++ app/src/main/java/com/osu/kusold/roar/CreateEventActivity.java
@@ -1,25 +1,108 @@
 package com.osu.kusold.roar;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import com.firebase.client.Firebase;
+import java.io.ByteArrayOutputStream;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.content.Intent;
 
 public class CreateEventActivity extends ActionBarActivity {
+
+    private Firebase fRef;
+    private String[] mDrawerTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private android.support.v7.widget.Toolbar mToolbar;
+    private Spinner mCategoryOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        // category options
+        mCategoryOptions = (Spinner) findViewById(R.id.categoryOptions);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mCategoryOptions.setAdapter(adapter);
+
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerTitles = getResources().getStringArray(R.array.drawer_option_strings);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mDrawerTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_closed) {
+            // Called when a drawer has settled in a completely closed state.
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            // Called when a drawer has settled in a completely open state.
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if(getSupportActionBar() != null) {
+                    getSupportActionBar().setTitle("Roar");
+                }
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if(getSupportActionBar() != null) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setLogo(R.drawable.ic_launcher);
+            actionBar.setDisplayUseLogoEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_event, menu);
+        //getMenuInflater().inflate(R.menu.menu_create_event, menu);
         return true;
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position, id);
+        }
+    }
+
+
+    // Swaps fragments in the main content view
+    private void selectItem(int position, long id) {
+        // Highlight the selected item, update the title, and close the drawer
+        if(position == 3) {
+            logout();
+        } else {
+            setTitle(mDrawerTitles[position]);
+        }
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 
     @Override
@@ -28,12 +111,20 @@ public class CreateEventActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
+        else if (id == R.id.action_create_event){
+            Intent intent = new Intent(CreateEventActivity.this, CreateEventActivity.class);
+            startActivity(intent);
+        }
         return super.onOptionsItemSelected(item);
     }
+
+    private void logout() {
+        fRef.unauth();
+        Intent intent = new Intent(CreateEventActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
+
--- app/src/main/java/com/osu/kusold/roar/EventFeedActivity.java
+++ app/src/main/java/com/osu/kusold/roar/EventFeedActivity.java
@@ -14,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.firebase.client.Firebase;
 
