f7644ceaf8f7768be67ab094103fe0550bba63ff
Johns Gresham <johnsgresham@gmail.com>
2015-04-02 09:36:23 -0400

    adding addition event info in query and created custom listener.

--- app/src/main/java/com/osu/kusold/roar/CreateEventActivity.java
+++ app/src/main/java/com/osu/kusold/roar/CreateEventActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -27,18 +26,6 @@ import com.firebase.client.Firebase;
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 public class CreateEventActivity extends ActionBarActivity {
