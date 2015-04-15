f026636b0de6b7cdd6c6a6afa88cfc91e38b52ee
Michelle Kusold <kusrox@gmail.com>
2015-04-15 03:33:57 -0400

    Fixing EventFeed so it doesn't crash from image issues

--- app/src/main/java/com/osu/kusold/roar/EventPostAdapter.java
+++ app/src/main/java/com/osu/kusold/roar/EventPostAdapter.java
@@ -3,6 +3,8 @@ package com.osu.kusold.roar;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +22,7 @@ public class EventPostAdapter extends ArrayAdapter<EventPost> {
 
     Context context;
     List<EventPost> posts;
+    ImageView mPostImageView;
 
     public EventPostAdapter(Context context, List<EventPost> posts) {
         super(context, R.layout.event_post_layout, posts);
@@ -31,18 +34,67 @@ public class EventPostAdapter extends ArrayAdapter<EventPost> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.event_post_layout, parent, false);
-        ImageView mPostImageView = (ImageView) rowView.findViewById(R.id.event_post_image);
+        mPostImageView = (ImageView) rowView.findViewById(R.id.event_post_image);
         TextView mPostNameView = (TextView) rowView.findViewById(R.id.event_post_name);
         TextView mPostDescriptionView = (TextView) rowView.findViewById(R.id.event_post_description);
 
         EventPost post = posts.get(position);
 
+        // set the brief event description listed in the eventfeed
         mPostNameView.setText(post.eventName);
         mPostDescriptionView.setText(post.description);
-        byte[] bitmapData = Base64.decode(post.image, Base64.DEFAULT);
-        Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapData, 0, bitmapData.length);
-        mPostImageView.setImageBitmap(bitmap);
+
+        Drawable thumbnail = decodeBase64(post.image);
+        mPostImageView.setImageDrawable(thumbnail);
 
         return rowView;
+    }
+
+    public Drawable decodeBase64(String input)
+    {
+        // preventing memory issues:
+        // Decode with inJustDecodeBounds=true to check dimensions
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        byte[] decodedByte = Base64.decode(input, 0);
+        BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length, options);
+
+        // find dimensions we want to scale to
+        int imgViewHeight = 70;
+        int imgViewWidth = 70;
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, imgViewHeight, imgViewWidth);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        Bitmap bImg = BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length, options);
+
+        Drawable d = new BitmapDrawable(getContext().getResources(),bImg);
+        return d;
+    }
+
+    /* Caculates a sample size value that is a power of two based on a target width and height */
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
